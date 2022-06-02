9128c229d23c5dba495f1dd91743466784ed6452
Artem Kashuta <kashuta@gmail.com>
2022-06-02 08:07:59 +0300

    feat: Button component ready

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Button from "./Components/Button/Button";
 
 function App() {
     return (
-        'work'
+        <>
+            <Button title={"Add"} disable={false} type={'add'}/>
+            <Button title={"Remove"} disable={false} type={'remove'}/>
+            <Button title={"Checkout"} disable={false} type={'checkout'}/>
+
+        </>
     );
 }
 
